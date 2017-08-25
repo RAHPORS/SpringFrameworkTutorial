ccc0d024b850ec817427d71f4dd42acfed1eb673
rahs2406 <ramon.junio94@gmail.com>
2017-08-25 16:40:18 -0500

    XML Configuration with Constructor

---
+++ src/main/java/com/rahpors/beans/Persona.java
@@ -0,0 +1,49 @@
+package com.rahpors.beans;
+
+/**
+ * Created by ramon on 25/08/17.
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apodo;
+
+    public Persona(int id, String nombre, String apodo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apodo = apodo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apodo='" + apodo + '\'' +
+                '}';
+    }
+}
